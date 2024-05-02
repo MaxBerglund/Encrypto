0e70d17a9d90f60fcd02009ff5343edb146a9d0c
maxb <maxbergl@kth.se>
2024-05-02 20:25:47 +0200

    Complete toHex() method

--- src/Encrypto.java
+++ src/Encrypto.java
@@ -1,11 +1,30 @@
 public class Encrypto {
-    private String key;
-    private String message;
 
     private int[] keyTransformer(String key) {
+        // NOT DONE
         // transform the key into hexadecimal, then binary, then put the binary digits into the following array
+        String hexKey = toHex(key);
+        
         int[] keyBinaryArray = {1};
 
         return keyBinaryArray;
+    }
+
+    public static String toHex(String text) {
+        String hex = "";
+        for (int i = 0; i < text.length(); i++) {
+            char ch = text.charAt(i);
+            int asciiOfCh = (int)ch;
+            String part = Integer.toHexString(asciiOfCh);
+            hex += part;
+        }
+        return hex;
+    }
+
+    public static void main(String[] args) {
+        String key = "abcdefg";
+        
+        String hexTest = toHex(key);
+        System.out.println(hexTest);
     }
 }
