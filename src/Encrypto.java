5849a77dbef254fc052a262fe492ff5687771353
maxb <maxbergl@kth.se>
2024-05-02 17:22:40 +0200

    Setup fill

--- src/Encrypto.java
+++ src/Encrypto.java
@@ -1,1 +1,11 @@
+public class Encrypto {
+    private String key;
+    private String message;
 
+    private int[] keyTransformer(String key) {
+        // transform the key into hexadecimal, then binary, then put the binary digits into the following array
+        int[] keyBinaryArray = {1};
+
+        return keyBinaryArray;
+    }
+}
