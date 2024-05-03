a430b8bc3c9864b22ea1204f8f41afb4125e9321
Abdelrahman Aldaker <aldaker@kth.se>
2024-05-03 15:10:11 +0200

    Add permute and printBits functions

--- src/Functions.java
+++ src/Functions.java
@@ -2,10 +2,45 @@
  * This file is used to store functions that are universally useful for this project.
  */
 public class Functions {
-    
-    // Permute function
-    // ...
-    
+
+        /**
+         * A function to permute an input array based on a permutation table
+         * 
+         * @param input The input array of bits (integers 0 or 1).
+         * @param permutationTable The permutation table that defins the output 
+         * @return The permuted array
+         */
+
+        public int[] permute(int[] input, int[] permutationTable)
+        {
+                int[] output = new int[permutationTable.length];
+
+                for(int i = 0;i < permutationTable.length;i++)
+                {
+                        output[i] = input[permutationTable[i]- 1];
+                }
+
+                return output;
+        }
+
+        /**
+         * Prints out the bits with a space after every 8th bit
+         * 
+         * @param bits
+         */
+        public void printBits(int[] bits)
+        {
+                for(int i = 0; i < bits.length;i++)
+                {
+                        System.out.println(bits[i]);
+                        if((i+1) % 8 == 0) System.out.print(" "); //spACE BETWEEN every 8th bit
+                }
+
+                System.out.println();
+        }
+
+
+
     /**
      * The XOR logical operation "Either or..." that is performed on two binary arrays and the results are given as a new binary array.
      * @param array1 consisting of 0:s and/or 1:s.
