fdbe6c102a948a989746ca0c3c4538e1762d86e0
maxb <maxbergl@kth.se>
2024-05-03 14:47:07 +0200

    Create public organisational files Functions.java, Converters.java and Tables.java

--- src/Converters.java
+++ src/Converters.java
@@ -1,1 +1,78 @@
+import java.util.HashMap;
 
+/**
+ * This file is used to store all convertion methods that are used to convert a message to hexadecimal or to binary, and such.
+ */
+public class Converters {
+
+    /**
+     * Converts a string into its hexadecimal representation using ascii values for its characters.
+     * @param text
+     * @return hexadecimal representation of text.
+     */
+    public static String string2Hex(String text) {
+        String hex = "";
+        for (int i = 0; i < text.length(); i++) {
+            char ch = text.charAt(i);
+            int asciiOfCh = (int)ch;
+            String part = Integer.toHexString(asciiOfCh);
+            hex += part;
+        }
+        return hex;
+    }
+
+    /**
+     * Converts a hexadecimal string into its binary representation.
+     * @param hex
+     * @return binary representation of the hex.
+     */
+    public static String hex2Binary(String hex) {
+        HashMap<Character, String> hashMap = new HashMap<Character, String>();
+
+        hashMap.put('0', "0000");
+        hashMap.put('1', "0001");
+        hashMap.put('2', "0010");
+        hashMap.put('3', "0011");
+        hashMap.put('4', "0100");
+        hashMap.put('5', "0101");
+        hashMap.put('6', "0110");
+        hashMap.put('7', "0111");
+        hashMap.put('8', "1000");
+        hashMap.put('9', "1001");
+        hashMap.put('a', "1010");
+        hashMap.put('b', "1011");
+        hashMap.put('c', "1100");
+        hashMap.put('d', "1101");
+        hashMap.put('e', "1110");
+        hashMap.put('f', "1111");
+
+        String binary = "";
+        for (int i = 0; i < hex.length(); i++) {
+            char ch = hex.charAt(i);
+            if (hashMap.containsKey(ch))
+                binary += hashMap.get(ch);
+            else {
+                binary = "Invalid Hexadecimal String";
+                return binary;
+            }
+        }   
+        return binary;
+    }
+
+    private static String string2Binary(String input){
+
+        StringBuilder binary = new StringBuilder();
+        
+        for(char character : input.toCharArray()) 
+        {
+
+            String binaryChar = Integer.toBinaryString(character);
+            String paddedBinaryChar = String.format("%8s", binaryChar).replace(' ', '0');
+
+            binary.append(paddedBinaryChar);
+            
+        }
+
+        return binary.toString();
+    }
+}
--- src/Functions.java
+++ src/Functions.java
@@ -1,1 +1,29 @@
+/**
+ * This file is used to store functions that are universally useful for this project.
+ */
+public class Functions {
+    
+    // Permute function
+    // ...
+    
+    /**
+     * The XOR logical operation "Either or..." that is performed on two binary arrays and the results are given as a new binary array.
+     * @param array1 consisting of 0:s and/or 1:s.
+     * @param array2 consisting of 0:s and/or 1:s.
+     * @return An array containing the XOR results.
+     */
+    public int[] xOr(int[] array1, int[] array2) {
+        int resultSize = array1.length;
+        int[] resultArray = new int[resultSize];
 
+        for (int i = 0; i < resultSize - 1; i++) {
+                if (array1[i] + array2[i] == 0 || array1[i] + array2[i] == 2) {
+                        resultArray[i] = 1;
+                } else {
+                        resultArray[i] = 0;
+                }
+        }
+        return resultArray;
+    }
+    
+}
