9e74729b0bd7d151228ac82d9461db57fa84e405
The378Swe <168176612+The378Swe@users.noreply.github.com>
2024-05-13 15:59:04 +0200

    Adding decryption of result of the encrypted message

--- src/Encrypto.java
+++ src/Encrypto.java
@@ -6,6 +6,7 @@ public class Encrypto {
     static KeyTransformer KeyClass = new KeyTransformer();
     static StringDivider StringDiv = new StringDivider();
     static ExpansionPermutation encrypt = new ExpansionPermutation();
+    static Decryption decryption = new Decryption();
 
     public static void main(String[] args) {
 
@@ -28,28 +29,36 @@ public class Encrypto {
         String key = scanner.nextLine();
         scanner.close();
 
-        // The following strings are the message and the key from the superb tutorial video (https://www.youtube.com/watch?v=-j80aA8q_IQ), used for testing this file.
-        // String message = "#Eg«Íï"; // Taken from 20:58 in the video. 0000 0001 0010 0011 0100 0101 0110 0111 1000 1001 1010 1011 1100 1101 1110 1111
+        // The following strings are the message and the key from the superb tutorial
+        // video (https://www.youtube.com/watch?v=-j80aA8q_IQ), used for testing this
+        // file.
+        // String message = "#Eg«Íï"; // Taken from 20:58 in the video. 0000 0001 0010
+        // 0011 0100 0101 0110 0111 1000 1001 1010 1011 1100 1101 1110 1111
         // String key = "4Wy¼ßñ"; // Taken from 7:24 in the video.
         // This message and key should produce the following hexadecimal ciphertext:
         // 85E813540F0AB405, taken from 48:31 in the video.
-        // This does though not work for our program, and we assume that is because this message and key are using symbols that are not compatible with out program.
+        // This does though not work for our program, and we assume that is because this
+        // message and key are using symbols that are not compatible with out program.
 
         // initilize som variables to convert blocks to array of ints of length 64
         int[][] KeysArray = KeyClass.keyTransformer(key);
         String[] blocks = StringDiv.divideIntoBlocks(message);
-        int[][] results = new int[blocks.length][64];
         int countBlock = 0;
-        int countPos = 0;
         String[] EncryptedString = new String[blocks.length];
+        String[] DecryptedString = new String[blocks.length];
 
         for (int i = 0; i < blocks.length; i++) {
             EncryptedString[i] = "";
         }
 
+        for (int i = 0; i < blocks.length; i++) {
+            DecryptedString[i] = "";
+        }
+
         // converts the blocks to array of ints and parses them to the encryption
         // function from the ExpansionPermutation class
         for (String block : blocks) {
+            String binaryString = "";
             int[] bitArray = new int[64];
 
             for (int i = 0; i < 64; i++) {
@@ -57,30 +66,23 @@ public class Encrypto {
             }
 
             int[] encryptedArray = encrypt.encryption(bitArray, KeysArray);
+            int[] decryptedArray = decryption.decryption(encryptedArray, KeysArray);
 
             for (int bit : encryptedArray) {
-                results[countBlock][countPos] = bit;
-                countPos++;
+                binaryString += bit;
             }
+            EncryptedString[countBlock] = converter.binary2Hex(binaryString);
 
+            binaryString = "";
+            for (int bit : decryptedArray) {
+                binaryString += bit;
+            }
+            DecryptedString[countBlock] = converter.binary2Hex(binaryString);
             countBlock++;
-            countPos = 0;
         }
-        
-        // chat-gpt generated for loop
-        System.out.println();
-        for (int i = 0; i < results[0].length; i++) {
-            for (int j = 0; j < EncryptedString.length; j++) {
-                EncryptedString[j] += results[j][i];
-            }
-        }
+        System.out.println("Encrypted: " + EncryptedString[0]);
+        System.out.println("Decrypted: " + DecryptedString[0]);
 
-        // print out the results from encrypting the string parsed
-        System.out.println("Ciphertext in hexadecimal format: ");
-        for (String s : EncryptedString) {
-            System.out.print(converter.binary2Hex(s));
-        }
-        System.out.println();
     }
 
     private static void printWithDelay(String data, int delay) {
