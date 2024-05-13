02c8a3e966a61de33cb1c878bd2f32bc94cd992f
The378Swe <168176612+The378Swe@users.noreply.github.com>
2024-05-13 18:37:03 +0200

    Fixing Decryption
    the int started at 1 before, it will only generate 15 keys because java is 0 indexed and stuff.

--- src/Decryption.java
+++ src/Decryption.java
@@ -4,7 +4,7 @@ public class Decryption {
     public int[] decryption(int[] array, int[][] KeysArray) {
         int[][] shiftedKeys = new int[16][48];
 
-        for (int i = 1; i < 16; i++) {
+        for (int i = 0; i < 16; i++) {
             shiftedKeys[i] = KeysArray[15 - i];
 
         }
