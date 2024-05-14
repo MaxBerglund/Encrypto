c071e190dcfd1e839d042eafa936379374aeda34
Abood Aldaker <abooddaker@gmail.com>
2024-05-15 00:34:37 +0200

    Add GUI and new Main.java folder that runs the project.

--- src/Encrypto.java
+++ src/Encrypto.java
@@ -78,56 +78,80 @@ public class Encrypto {
 
             }
         }
-
-        // The following strings are the message and the key from the superb tutorial
-        // video (https://www.youtube.com/watch?v=-j80aA8q_IQ), used for testing this
-        // file.
-        // String message = "#Eg«Íï"; // Taken from 20:58 in the video. 0000 0001 0010
-        // 0011 0100 0101 0110 0111 1000 1001 1010 1011 1100 1101 1110 1111
-        // String key = "4Wy¼ßñ"; // Taken from 7:24 in the video.
-        // This message and key should produce the following hexadecimal ciphertext:
-        // 85E813540F0AB405, taken from 48:31 in the video.
-        // This does though not work for our program, and we assume that is because this
-        // message and key are using symbols that are not compatible with out program.
-
-        // initilize som variables to convert blocks to array of ints of length 64
-
-        /*
-         * tests
-         * String key1 = "abcdefgh";
-         * String stringMessage =
-         * "I am well aware that my cat is super large with tiny little legs";
-         * 
-         * String hexMessage =
-         * "d4e33bbf95dca8fc9d4e50254157d5a162d746151dcc208f4faeb24f9803ecb064d2d2788d325da67b42027771b7355007cfecccafb1af16a93e7731dc60d7b7";
-         * 
-         * String encrypted = formatingResults.format(key1, stringMessage, true);
-         * 
-         * String decryptedDirect = formatingResults.format(key1, encrypted, false);
-         * 
-         * String decrypted = formatingResults.format(key1, hexMessage, false);
-         * 
-         * System.out.println("Decrypted: " + decrypted);
-         * 
-         * System.out.println("Decrypted Direct: " + decryptedDirect);
-         * 
-         * System.out.println("Encrypted: " + encrypted);
-         */
-
-        /*
-         * for (String blocks : encrypted){
-         * System.out.print(blocks);
-         * }
-         * 
-         * System.out.println();
-         * 
-         * System.out.println("Decrypted: ");
-         * 
-         * for (String blocks : decrypted){
-         * System.out.print(blocks);
-         * }
-         */
     }
+
+
+    /**
+     * Encyrpts a message with a specified key
+     * 
+     * @param message The message to encrypt
+     * @param key THe encryption key (8 chars)
+     * @return The encrypted message in hexadecimal 
+     */
+    public static String encryptMessage(String message, String key) {
+        return formatingResults.format(key, message, true);
+    }
+    /**
+     * Decrypts a hex string with a specified key
+     * 
+     * @param hexMessage The hex message to decrypt
+     * @param key The decryption key (8 chars)
+     * @return The decrypted message in text
+     */
+    public static String decryptMessage(String hexMessage, String key) {
+        return formatingResults.format(key, hexMessage, false);
+    }
+
+
+//         // The following strings are the message and the key from the superb tutorial
+//         // video (https://www.youtube.com/watch?v=-j80aA8q_IQ), used for testing this
+//         // file.
+//         // String message = "#Eg«Íï"; // Taken from 20:58 in the video. 0000 0001 0010
+//         // 0011 0100 0101 0110 0111 1000 1001 1010 1011 1100 1101 1110 1111
+//         // String key = "4Wy¼ßñ"; // Taken from 7:24 in the video.
+//         // This message and key should produce the following hexadecimal ciphertext:
+//         // 85E813540F0AB405, taken from 48:31 in the video.
+//         // This does though not work for our program, and we assume that is because this
+//         // message and key are using symbols that are not compatible with out program.
+
+//         // initilize som variables to convert blocks to array of ints of length 64
+
+//         /*
+//          * tests
+//          * String key1 = "abcdefgh";
+//          * String stringMessage =
+//          * "I am well aware that my cat is super large with tiny little legs";
+//          * 
+//          * String hexMessage =
+//          * "d4e33bbf95dca8fc9d4e50254157d5a162d746151dcc208f4faeb24f9803ecb064d2d2788d325da67b42027771b7355007cfecccafb1af16a93e7731dc60d7b7";
+//          * 
+//          * String encrypted = formatingResults.format(key1, stringMessage, true);
+//          * 
+//          * String decryptedDirect = formatingResults.format(key1, encrypted, false);
+//          * 
+//          * String decrypted = formatingResults.format(key1, hexMessage, false);
+//          * 
+//          * System.out.println("Decrypted: " + decrypted);
+//          * 
+//          * System.out.println("Decrypted Direct: " + decryptedDirect);
+//          * 
+//          * System.out.println("Encrypted: " + encrypted);
+//          */
+
+//         /*
+//          * for (String blocks : encrypted){
+//          * System.out.print(blocks);
+//          * }
+//          * 
+//          * System.out.println();
+//          * 
+//          * System.out.println("Decrypted: ");
+//          * 
+//          * for (String blocks : decrypted){
+//          * System.out.print(blocks);
+//          * }
+//          */
+//     }
 
     private static void printWithDelay(String data, int delay) {
         for (char character : data.toCharArray()) {
---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+//main class that runs the whole project (trying to fix that)
+    public static void main(String[] args) {
+        EncryptoGUI.launch();
+    }
+}
