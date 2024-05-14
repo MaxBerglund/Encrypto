570275fc5e8e141d2608dd3e37d9eee49b5f9e0a
The378Swe <168176612+The378Swe@users.noreply.github.com>
2024-05-14 15:58:35 +0200

    Changing string divider to work with the new formater java class
    removed the first converter in stringdivider so that the method only takes in a binary string so I can convert in advance to allow decryption to take hex and encryption string (I prepare the binary string in advance)

--- src/StringDivider.java
+++ src/StringDivider.java
@@ -2,29 +2,27 @@ public class StringDivider {
     static Converters converter = new Converters();
 
     /**
-     * Divides the input text that is to be encrypted into binary blocks that are length 64-bit each.
+     * Divides the input text that is to be encrypted into binary blocks that are
+     * length 64-bit each.
      * Created for the DES encryption algorithm.
      * 
      * @param input The input string to be encrypted
-     * @return An array containing the divided input which consists of 64-bit arrays of binary strings.
+     * @return An array containing the divided input which consists of 64-bit arrays
+     *         of binary strings.
      */
-    public static String[] divideIntoBlocks(String input)
-    {
+    public static String[] divideIntoBlocks(String binaryString) {
         // Convert to binary.
-        String binaryString = converter.string2Binary(input);
 
         int nrOfBlocks = (int) Math.ceil(binaryString.length() / 64.0);
 
         String[] blocks = new String[nrOfBlocks];
 
-        for(int i = 0; i < nrOfBlocks; i++)
-        {
+        for (int i = 0; i < nrOfBlocks; i++) {
             int start = i * 64;
-            int end = Math.min((i+1) * 64, binaryString.length());
+            int end = Math.min((i + 1) * 64, binaryString.length());
             blocks[i] = binaryString.substring(start, end);
 
-            if(blocks[i].length() < 64)
-            {
+            if (blocks[i].length() < 64) {
                 blocks[i] = fillWithZeros(blocks[i], 64);
             }
         }
@@ -34,44 +32,42 @@ public class StringDivider {
     }
 
     /**
-     * Fills the remaining part of the block with zeros. This might not be an official part of the DES encryption algorithm, but we think that this seems reasonable.
+     * Fills the remaining part of the block with zeros. This might not be an
+     * official part of the DES encryption algorithm, but we think that this seems
+     * reasonable.
      * 
      * @param binary the binary string to fill with zeros
      * @param length the length of the string to check if its less than 64
-     * @return The same initial binary string, but with the empty space at the end filled with zeros.
+     * @return The same initial binary string, but with the empty space at the end
+     *         filled with zeros.
      */
-    private static String fillWithZeros(String binary, int length)
-    {
-        while(binary.length() < length)
-        {
-        binary = binary + "0";
+    private static String fillWithZeros(String binary, int length) {
+        while (binary.length() < length) {
+            binary = binary + "0";
         }
 
         return binary;
     }
 
     public static void main(String[] args) {
-        
+
         String testString = "Encrypto stringggg";
 
         String[] blocks = divideIntoBlocks(testString);
-        
-        for(String block : blocks)
-        {
+
+        for (String block : blocks) {
             System.out.print(block);
-            System.out.println(" ["+block.length()+" bits]");
+            System.out.println(" [" + block.length() + " bits]");
         }
 
         System.out.println();
 
-        
         String testString2 = "Encrypto stringggg";
         String[] blocks2 = divideIntoBlocks(testString2);
 
-        for(String block : blocks2)
-        {
+        for (String block : blocks2) {
             System.out.print(block);
-            System.out.println(" ["+block.length()+" bits]");
+            System.out.println(" [" + block.length() + " bits]");
         }
     }
 
