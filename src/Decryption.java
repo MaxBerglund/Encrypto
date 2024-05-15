c337b24dd82571e14aa7ce8a34fad117e3a67611
The378Swe <168176612+The378Swe@users.noreply.github.com>
2024-05-15 11:55:35 +0200

    adding documentation Decryption.java

--- src/Decryption.java
+++ src/Decryption.java
@@ -1,6 +1,16 @@
 public class Decryption {
     ExpansionPermutation encrypt = new ExpansionPermutation();
 
+    /**
+     * decryptes a binary array using the same key as when the binary array was
+     * encrypted.
+     * 
+     * @param array     an encrypted binary array to be decrypted, must have the
+     *                  length 64.
+     * @param KeysArray an array of arrays with size [16][48], in other words, 16
+     *                  keys (binary arrays) of length 48 consisting.
+     * @return returns a decrypted binary array
+     */
     public int[] decryption(int[] array, int[][] KeysArray) {
 
         int[][] shiftedKeys = new int[16][48];
